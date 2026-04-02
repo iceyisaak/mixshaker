a19cd425b446cf169b805be2ba5627838b6ca7c9
iceyisaak <me@iceyisaak.com>
2026-04-02 16:53:08 +0200

    Added Home

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
+import { Home } from "./pages/home";
+
 function App() {
-  return <h1 className="text-3xl font-bold text-blue-500">Hello, Tailwind!</h1>;
+  return <Home />;
 }
 
 export default App;
