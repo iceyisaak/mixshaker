02e3318c249ed0ee0abbee19967192f4a9d38cc6
iceyisaak <me@iceyisaak.com>
2026-04-02 18:04:46 +0200

    Added DetailPage

--- src/sections/hero-section/hero-section.tsx
+++ src/sections/hero-section/hero-section.tsx
@@ -2,7 +2,7 @@ import { SearchBar } from "../../components/search-bar";
 
 export const HeroSection = () => {
   return (
-    <section>
+    <section className="">
       <h1>Get your new Cocktail Recipe</h1>
       <SearchBar />
     </section>
